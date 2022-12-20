193394db3488d0e0c36a5f750752452c190a105f
Pato Kuack <nocturno_x_13514@hotmail.com>
2022-12-19 18:40:40 -0600

    3°-1 Se agregaron los NavLinks a la barra de navegación.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import championStats from '../containers/ChampionStats.jsx';
 
 const Header = () => {
@@ -6,8 +7,16 @@ const Header = () => {
     <nav className="fixed top-0 flex flex-row top-0 w-full h-fit p-2 bg-gray-800">
       <ul>
         <li className="inline px-4 py-2 hover:bg-gray-600"><a href="/">Login</a></li>
-        <li className="inline px-4 py-2 hover:bg-gray-600"><a href="./championstats">Estadisticas por Campeon</a></li>
-        <li className="inline px-4 py-2 hover:bg-gray-600"><a href="./championstats">Estadisticas por Carril</a></li>
+        <li className="inline px-4 py-2 hover:bg-gray-600">
+          <NavLink to={ '/championstats' }>
+            Estadisticas por Campeon
+          </NavLink>
+        </li>
+        <li className="inline px-4 py-2 hover:bg-gray-600">
+          <NavLink to={ '/championstats' }>
+            Estadisticas por Carril
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
