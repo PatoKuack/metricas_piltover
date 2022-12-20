0922e7077fc5784fed89dbc3c2328ab56ac5e286
Pato Kuack <nocturno_x_13514@hotmail.com>
2022-12-19 18:42:00 -0600

    3°-2 Se m olvidó guardar. XD.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,11 @@ const Header = () => {
   return (
     <nav className="fixed top-0 flex flex-row top-0 w-full h-fit p-2 bg-gray-800">
       <ul>
-        <li className="inline px-4 py-2 hover:bg-gray-600"><a href="/">Login</a></li>
+        <li className="inline px-4 py-2 hover:bg-gray-600">
+          <NavLink to={ '/' }>
+            Login
+          </NavLink>
+        </li>
         <li className="inline px-4 py-2 hover:bg-gray-600">
           <NavLink to={ '/championstats' }>
             Estadisticas por Campeon
