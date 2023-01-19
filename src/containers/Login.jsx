3886e749fc4f69093397420919d9420b3aaad5aa
Pato Kuack <nocturno_x_13514@hotmail.com>
2023-01-18 22:15:17 -0600

    6°-14 Se actualizó la key.

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
 
   // const API_KEY = `${process.env.API_KEY}`;
   // console.log(`1) ...${process.env.OSO}`);
-  const API_KEY = 'RGAPI-a5517aa3-5c66-4994-aa2c-c6198fd12c51';
+  const API_KEY = 'RGAPI-11f19ebd-45b9-47f7-b74e-e0e7a4fde776';
 
   const {
     lastVersion,
